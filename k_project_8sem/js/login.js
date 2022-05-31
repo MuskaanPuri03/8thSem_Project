2fdb024b76dcd19a4cf9e831f698ada272f2acd7
MuskaanPuri03 <98894992+MuskaanPuri03@users.noreply.github.com>
2022-05-31 21:26:49 +0530

    Add files via upload

---
+++ k_project_8sem/js/login.js
@@ -0,0 +1,17 @@
+function check(form) {
+    if(form.usrname.value == "Admin" && form.psw.value == "Admin123")
+     {
+        alert('Successfully Login');
+        window.open("admin.html")
+    }
+    else if(form.usrname.value == "Shilpa" && form.psw.value == "Shilpa123")   
+     {
+        alert('Successfully Login');
+         window.open('selectbank.html')
+     }
+      else
+       {
+         alert("Error. Please enter correct Username");
+       }
+      }
+       
